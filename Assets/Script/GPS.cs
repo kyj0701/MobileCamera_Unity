e4767703ba01f04304427cd26e60f3ef4d44c7ec
Yejun Kim <yejun283@gmail.com>
2021-10-29 16:37:41 +0900

    Update real-time GPS

--- Assets/Script/GPS.cs
+++ Assets/Script/GPS.cs
@@ -8,6 +8,7 @@ public class GPS : MonoBehaviour {
 
     public static GPS Instance { set; get; }
 
+    public Text TF;
     public float latitude;
     public float longitude;
     public float altitude;
@@ -15,55 +16,63 @@ public class GPS : MonoBehaviour {
     public float VDOP;
     public float HDOP;
     public float PDOP;
-
-
-    void Awake()
-    {
-        if (!Permission.HasUserAuthorizedPermission("android.permission.ACCESS_FINE_LOCATION"))
-        {
-            Permission.RequestUserPermission("android.permission.ACCESS_FINE_LOCATION");
-        }
-    }
+    public int maxWait = 5;
+    public int delay;
 
     void Start()
     {
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        // StartCoroutine(StartLocationService());
+    }
+
+    void Update()
+    {
         StartCoroutine(StartLocationService());
     }
 
     private IEnumerator StartLocationService()
     {
+        if (!Permission.HasUserAuthorizedPermission("android.permission.ACCESS_FINE_LOCATION"))
+        {
+            Permission.RequestUserPermission("android.permission.ACCESS_FINE_LOCATION");
+            while (!Permission.HasUserAuthorizedPermission("android.permission.ACCESS_FINE_LOCATION"))
+            {
+                yield return null;
+            }
+        }
+
         if (!Input.location.isEnabledByUser)
         {
             Debug.Log("User has not enabled GPS");
-            yield break;
         }
 
         Input.location.Start();
-        int maxWait = 100;
         
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        while (Input.location.status == LocationServiceStatus.Initializing && delay < maxWait)
         {
             Debug.Log("HERE?");
-            yield return new WaitForSeconds(1);
-            maxWait--;
+            TF.text = "GPS Update Complete";
+            yield return new WaitForSeconds(3);
+            delay++;
         }
 
-        if (maxWait <= 0)
+        if (Input.location.status == LocationServiceStatus.Failed || Input.location.status == LocationServiceStatus.Stopped)
         {
-            Debug.Log("Timed out");
-            yield break;
-        }
-
-        if (Input.location.status == LocationServiceStatus.Failed)
-        {
+            TF.text = "GPS Update failed";
             Debug.Log("Unable to determine device location");
-            yield break;
+        }
+
+        if (delay >= maxWait)
+        {
+            TF.text = "Time out";
+            Debug.Log("Time out");
         }
         
-        Debug.Log(altitude);
         UpdateLocationInfo();
+        TF.text = "GPS Update";
+        Debug.Log("GPS Update");
+        yield return new WaitForSeconds(2);
     }
 
     public void UpdateLocationInfo()
