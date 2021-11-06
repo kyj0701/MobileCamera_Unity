acb78a14731b1b83d862f8c97d1f3324cbf1366d
Yejun Kim <yejun283@gmail.com>
2021-11-06 16:31:58 +0900

    Synchro mobile camera and Unity camera

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,19 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject camParent;
     void Start()
     {
+        Input.gyro.enabled = true;
+        camParent = new GameObject("CamParent");
+        camParent.transform.position = this.transform.position;
+        this.transform.parent = camParent.transform;
         Input.gyro.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // transform.Rotate(Input.gyro.rotationRateUnbiased.x, Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z);
         // transform.position = new Vector3((GPS.Instance.longitude * 10000) % 100, (GPS.Instance.latitude * 10000) % 100, GPS.Instance.altitude);
+        camParent.transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        this.transform.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0);
     }
 }
--- Assets/Script/Client.cs
+++ Assets/Script/Client.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
